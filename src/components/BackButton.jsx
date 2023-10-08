fe47c1858c07bd7a35715d75a13f9b98dcb5b0c2
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-10-08 16:55:16 +0300

    second commit

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-
 const BackButton = () => {
   const history = useNavigate();
   const from = localStorage.getItem('from');
 
-  return <Button onClick={() => history(from)}>&#x2190; Go back</Button>;
+  return (
+    <button className="btn btn-secondary m-3" onClick={() => history(from)}>
+      &#x2190; Go back
+    </button>
+  );
 };
 
 export default BackButton;
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,10 @@
-import { Button } from 'react-bootstrap';
-
 const SearchForm = ({ onChange, value, search }) => {
   return (
     <form onSubmit={search}>
       <input onChange={onChange} value={value} type="text" name="query" />
-      <Button type="submit">Search</Button>
+      <button type="submit" className="btn btn-secondary m-3">
+        Search
+      </button>
     </form>
   );
 };
